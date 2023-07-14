2171db198ce8034d4745d83e24ca3dc181d6b874
Николай <ibraevnikolay@mail.ru>
2023-07-14 20:24:50 +0500

    дз по пятому семинару

---
+++ src/main/java/org/example/sem_5/homework/Main.java
@@ -0,0 +1,10 @@
+package org.example.sem_5.homework;
+
+import org.example.sem_5.homework.view.View;
+
+public class Main {
+    public static void main(String[] args) {
+        View view = new View();
+        view.start();
+    }
+}
