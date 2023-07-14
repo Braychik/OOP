80c43a6afea13b00a002b59a9c7f1092f7a901e0
Николай <ibraevnikolay@mail.ru>
2023-07-14 20:22:33 +0500

    дз по 5 семинару

---
+++ src/main/java/org/example/sem_5/homework/data/Komplex.java
@@ -0,0 +1,17 @@
+package org.example.sem_5.homework.data;
+
+public class Komplex {
+    public double a;
+    public double b;
+
+    public Komplex(double num1, double num2) {
+        a = num1;
+        b = num2;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%f + %fi", a, b);
+    }
+
+}
---
+++ src/main/java/org/example/sem_5/homework/data/Rational.java
@@ -0,0 +1,15 @@
+package org.example.sem_5.homework.data;
+
+public class Rational {
+    public int num1;
+    public int num2;
+
+    public Rational(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    @Override
+    public String toString() {
+        return num1 + "/" + num2;
+    }
+}
